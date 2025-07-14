7571c0c9930874baeebbca2b4cd8726b5b8b23f1
ardans <ardans@foxmail.com>
2025-07-14 12:16:22 +0800

    feat: add vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,33 @@
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+      ],
+      dirs: [
+        'src/composables',
+        'src/stores',
+        'src/utils',
+      ],
+      eslintrc: { enabled: true },
+    }),
+    Components({
+      dirs: [
+        'src/components',
+      ],
+      extensions: ['vue'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
