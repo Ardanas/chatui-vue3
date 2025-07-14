f6d30f3919f5a6c084dc23bb86323767d337cda2
ardans <ardans@foxmail.com>
2025-07-14 12:12:36 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue'
+</script>
+
+<template>
+  <div>
+    <Button>hello</Button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
